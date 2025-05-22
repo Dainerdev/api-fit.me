ebca7ec689239aa9c04432ff4095c58222d50955
Dainerdev <florianarroyo15@gmail.com>
2025-05-21 19:36:08 -0500

    Se implementa el manejo de las rutas para trabajar con los datos de usuarios

--- src/db/db.config.js
+++ src/db/db.config.js
@@ -1,22 +1,12 @@
 import mysql from 'promise-mysql';
-<<<<<<< HEAD
 import config from '../config';
-=======
-import { config } from 'dotenv';
->>>>>>> fa91f379fda4ca33d609b828d0dc157559579bb5
 
 // Load environment variables from .env file
 const connection = mysql.createConnection({
     host: config.host,
-<<<<<<< HEAD
     database: config.database,
     user: config.user,
     password: config.password
-=======
-    dbName: config.dbName,
-    dbUser: config.dbUser,
-    dbPassword: config.dbPassword
->>>>>>> fa91f379fda4ca33d609b828d0dc157559579bb5
 });
 
 // Connect to the database
