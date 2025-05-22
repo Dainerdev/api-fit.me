a1c764b51b9bde1de79ab37b0611dd4d1e2abee3
Dainerdev <florianarroyo15@gmail.com>
2025-05-21 21:04:47 -0500

    Se añaden rutas para las demás funciones del CRUD

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,5 +6,15 @@ const router = Router();
 // Routes
 // Get route
 router.get("/", userController.getUsers);
+router.get("/:id", userController.getUserById);
+
+// Post route
+router.post("/", userController.createUser);
+
+// Put route
+router.put("/:id", userController.updateUser);
+
+// Delete route
+router.delete("/:id", userController.deleteUser);
 
 export default router;
